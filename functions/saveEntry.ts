900c1feefa1f6ba8ef375f91e1cc6c28424e9214
Brahm Van Houzen <brahmvanh@gmail.com>
2024-01-18 16:17:48 -0500

    working on creating serverless functions

--- functions/saveEntry.ts
+++ functions/saveEntry.ts
@@ -3,17 +3,18 @@ import { Handler } from '@netlify/functions';
 import { MongoClient } from 'mongodb';
 
 const handler: Handler = async function (event, context) {
-	const { data } = JSON.parse(event.body);
-	const connectionString: string = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizzaTracker';
 
-	try {
-		const client = new MongoClient(connectionString);
-		await client.connect();
-
-		const database = client.db('yourDatabaseName');
-		const collection = database.collection('yourCollectionName');
-
-		await collection.insertOne({ data });
+    const { data } = JSON.parse(event.body);
+    const connectionString: string = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizzaTracker';
+    
+    try {
+      const client = new MongoClient(connectionString);
+      await client.connect();
+      
+      const database = client.db('yourDatabaseName');
+      const collection = database.collection('yourCollectionName');
+      
+      await collection.insertOne({ data });
 
 		return {
 			statusCode: 200,
