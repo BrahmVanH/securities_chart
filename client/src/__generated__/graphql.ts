d931d7ad60a6d15e0ef62b3da0c25108f59b3e9f
Brahm Van Houzen <brahmvanh@gmail.com>
2024-02-18 23:27:23 -0500

    wired up gql query to log. starting styling

--- client/src/__generated__/graphql.ts
+++ client/src/__generated__/graphql.ts
@@ -8,14 +8,14 @@ export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Mayb
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-  Date: { input: any; output: any; }
-};
+// export type Scalars = {
+//   ID: { input: string; output: string; }
+//   String: { input: string; output: string; }
+//   Boolean: { input: boolean; output: boolean; }
+//   Int: { input: number; output: number; }
+//   Float: { input: number; output: number; }
+//   Date: { input: any; output: any; }
+// };
 
 export type Entry = {
   __typename?: 'Entry';
@@ -27,40 +27,40 @@ export type Entry = {
   social?: Maybe<Scalars['Int']['output']>;
 };
 
-export type Mutation = {
-  __typename?: 'Mutation';
-  createEntry?: Maybe<Entry>;
-};
+// export type Mutation = {
+//   __typename?: 'Mutation';
+//   createEntry?: Maybe<Entry>;
+// };
 
 
-export type MutationCreateEntryArgs = {
-  dietary?: InputMaybe<Scalars['Int']['input']>;
-  financial?: InputMaybe<Scalars['Int']['input']>;
-  fitness?: InputMaybe<Scalars['Int']['input']>;
-  professional?: InputMaybe<Scalars['Int']['input']>;
-  social?: InputMaybe<Scalars['Int']['input']>;
-};
+// export type MutationCreateEntryArgs = {
+//   dietary?: InputMaybe<Scalars['Int']['input']>;
+//   financial?: InputMaybe<Scalars['Int']['input']>;
+//   fitness?: InputMaybe<Scalars['Int']['input']>;
+//   professional?: InputMaybe<Scalars['Int']['input']>;
+//   social?: InputMaybe<Scalars['Int']['input']>;
+// };
 
-export type Query = {
-  __typename?: 'Query';
-  allEntries?: Maybe<Array<Maybe<Entry>>>;
-};
+// export type Query = {
+//   __typename?: 'Query';
+//   allEntries?: Maybe<Array<Maybe<Entry>>>;
+// };
 
-export type CreateEntryMutationVariables = Exact<{
-  financial?: InputMaybe<Scalars['Int']['input']>;
-  fitness?: InputMaybe<Scalars['Int']['input']>;
-  dietary?: InputMaybe<Scalars['Int']['input']>;
-  social?: InputMaybe<Scalars['Int']['input']>;
-  professional?: InputMaybe<Scalars['Int']['input']>;
-}>;
-
-
-export type CreateEntryMutation = { __typename?: 'Mutation', createEntry?: { __typename?: 'Entry', date?: any | null, financial?: number | null, fitness?: number | null, dietary?: number | null, social?: number | null, professional?: number | null } | null };
-
-export type AllEntriesQueryVariables = Exact<{ [key: string]: never; }>;
+// export type CreateEntryMutationVariables = Exact<{
+//   financial?: InputMaybe<Scalars['Int']['input']>;
+//   fitness?: InputMaybe<Scalars['Int']['input']>;
+//   dietary?: InputMaybe<Scalars['Int']['input']>;
+//   social?: InputMaybe<Scalars['Int']['input']>;
+//   professional?: InputMaybe<Scalars['Int']['input']>;
+// }>;
 
 
-export type AllEntriesQuery = { __typename?: 'Query', allEntries?: Array<{ __typename?: 'Entry', date?: any | null, financial?: number | null, fitness?: number | null, dietary?: number | null, social?: number | null, professional?: number | null } | null> | null };
+// export type CreateEntryMutation = { __typename?: 'Mutation', createEntry?: { __typename?: 'Entry', date?: any | null, financial?: number | null, fitness?: number | null, dietary?: number | null, social?: number | null, professional?: number | null } | null };
+
+// export type AllEntriesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+// export type AllEntriesQuery = { __typename?: 'Query', allEntries?: Array<{ __typename?: 'Entry', date?: any | null, financial?: number | null, fitness?: number | null, dietary?: number | null, social?: number | null, professional?: number | null } | null> | null };
 
 
 export const CreateEntryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateEntry"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"financial"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"fitness"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"dietary"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"social"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"professional"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createEntry"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"financial"},"value":{"kind":"Variable","name":{"kind":"Name","value":"financial"}}},{"kind":"Argument","name":{"kind":"Name","value":"fitness"},"value":{"kind":"Variable","name":{"kind":"Name","value":"fitness"}}},{"kind":"Argument","name":{"kind":"Name","value":"dietary"},"value":{"kind":"Variable","name":{"kind":"Name","value":"dietary"}}},{"kind":"Argument","name":{"kind":"Name","value":"social"},"value":{"kind":"Variable","name":{"kind":"Name","value":"social"}}},{"kind":"Argument","name":{"kind":"Name","value":"professional"},"value":{"kind":"Variable","name":{"kind":"Name","value":"professional"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"date"}},{"kind":"Field","name":{"kind":"Name","value":"financial"}},{"kind":"Field","name":{"kind":"Name","value":"fitness"}},{"kind":"Field","name":{"kind":"Name","value":"dietary"}},{"kind":"Field","name":{"kind":"Name","value":"social"}},{"kind":"Field","name":{"kind":"Name","value":"professional"}}]}}]}}]} as unknown as DocumentNode<CreateEntryMutation, CreateEntryMutationVariables>;
@@ -75,15 +75,15 @@ export type Scalars = {
   Date: { input: any; output: any; }
 };
 
-export type Entry = {
-  __typename?: 'Entry';
-  date?: Maybe<Scalars['Date']['output']>;
-  dietary?: Maybe<Scalars['Int']['output']>;
-  financial?: Maybe<Scalars['Int']['output']>;
-  fitness?: Maybe<Scalars['Int']['output']>;
-  professional?: Maybe<Scalars['Int']['output']>;
-  social?: Maybe<Scalars['Int']['output']>;
-};
+// export type Entry = {
+//   __typename?: 'Entry';
+//   date?: Maybe<Scalars['Date']['output']>;
+//   dietary?: Maybe<Scalars['Int']['output']>;
+//   financial?: Maybe<Scalars['Int']['output']>;
+//   fitness?: Maybe<Scalars['Int']['output']>;
+//   professional?: Maybe<Scalars['Int']['output']>;
+//   social?: Maybe<Scalars['Int']['output']>;
+// };
 
 export type Mutation = {
   __typename?: 'Mutation';
--- client/src/components/Log.tsx
+++ client/src/components/Log.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useQuery } from '@apollo/client';
 import { GET_ALL_ENTRIES } from '../utils/queries';
 import { IChartData } from '../types';
+import { Entry } from '../__generated__/graphql';
 
 const EntriesContainer = styled.div`
 	width: 80%;
@@ -22,29 +23,50 @@ const EntriesContainer = styled.div`
 	}
 `;
 
+const EntryCard = styled.div`
+  width: 100%;
+  height: 10%;
+  border: 1px solid white;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+`;
+
 const Log = () => {
-	const [entries, setEntries] = useState<IChartData[] | null>(null);
+	const [entries, setEntries] = useState<Entry[] | null>(null);
 
 	const { data, loading, error } = useQuery(GET_ALL_ENTRIES);
 
 	useEffect(() => {
-		if (!loading && data) {
-			console.log(data);
+		if (!loading && data?.allEntries) {
+			console.log(data.allEntries);
+			setEntries(data.allEntries.filter((entry): entry is Entry => entry !== null));
 		} else if (error) {
 			console.log(error);
 			throw new Error('Error fetching entries');
 		}
-	});
+	}, []);
 	return (
 		<div>
 			<h1>Securities Log</h1>
-			{/* <EntriesContainer>
-				{entries.map((entry, index) => (
-					<Entry key={index}>
-						<p>{entry}</p>
-					</Entry>
-				))}
-			</EntriesContainer> */}
+			{entries ? (
+				<EntriesContainer>
+					{entries.map((entry, index) => (
+						<EntryCard key={index}>
+							<p style={{fontSize: '14px'}}>{entry.date}</p>
+              <p>{entry.dietary}</p>
+              <p>{entry.financial}</p> 
+              <p>{entry.fitness}</p>
+              <p>{entry.professional}</p>
+              <p>{entry.social}</p>
+						</EntryCard>
+					))}
+				</EntriesContainer>
+			) : (
+				<></>
+			)}
 		</div>
 	);
 };
