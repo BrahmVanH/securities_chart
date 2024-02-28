fcfe97fd52a3ce0a8a0c9f83b465269847eefaa6
Brahm Van Houzen <brahmvanh@gmail.com>
2024-02-27 22:18:15 -0500

    fixed error in handler function

--- server/lambda/graphql.ts
+++ server/lambda/graphql.ts
@@ -3,21 +3,21 @@ import { createLambdaServer } from './bundle/server';
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
 	try {
-
 		const server = createLambdaServer();
 		return new Promise((resolve, reject) => {
-		const cb = (error: string | Error | null | undefined, args: any) => {
-			if (error) {
-				console.error('error in handler: ', error);
-				reject(error);
-			} else {
-				resolve(args);
-			}
-		};
-		
-		server.createHandler()(event, context, cb);
-	});
-} catch (error) {
-	console.error('error in handler: ', error);
-	throw error;
+			const cb = (error: string | Error | null | undefined, args: any) => {
+				if (error) {
+					console.error('error in handler: ', error);
+					reject(error);
+				} else {
+					resolve(args);
+				}
+			};
+
+			server.createHandler()(event, context, cb);
+		});
+	} catch (error) {
+		console.error('error in handler: ', error);
+		throw error;
+	}
 };
