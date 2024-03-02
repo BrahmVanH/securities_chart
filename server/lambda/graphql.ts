e1d0df44208388b68f9429b2c30448ada91d06b5
Brahm Van Houzen <brahmvanh@gmail.com>
2024-03-01 20:30:40 -0500

    went with a more simplified handler to try

--- server/lambda/graphql.ts
+++ server/lambda/graphql.ts
@@ -1,22 +1,26 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { createLambdaServer } from './bundle/server';
 
-export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
-	try {
-		console.log('event: ', event);
-		console.log('context: ', context);
-		const server = createLambdaServer();
+// export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
+// 	try {
+// 		console.log('event: ', event);
+// 		console.log('context: ', context);
+// 		const server = createLambdaServer();
 
-		const result = new Promise((resolve, reject) => {
-			const cb = (err: Error, args: any) => (err ? reject(err) : resolve(args));
-			server.createHandler()(event, context, cb);
-		});
-		console.log('result: ', result);
-		const resolvedResult = await result;
-		console.log('resolvedResult: ', resolvedResult);
-		return result;
-	} catch (error) {
-		console.error('error in handler: ', error);
-		throw error;
-	}
-};
+// 		const result = new Promise((resolve, reject) => {
+// 			const cb = (err: Error, args: any) => (err ? reject(err) : resolve(args));
+// 			server.createHandler()(event, context, cb);
+// 		});
+// 		console.log('result: ', result);
+// 		const resolvedResult = await result;
+// 		console.log('resolvedResult: ', resolvedResult);
+// 		return result;
+// 	} catch (error) {
+// 		console.error('error in handler: ', error);
+// 		throw error;
+// 	}
+// };
+
+const graphqlServer = createLambdaServer();
+
+export const handler = graphqlServer.createHandler();
